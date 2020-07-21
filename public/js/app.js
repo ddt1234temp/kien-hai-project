884c4fb85ea243fbda5ce2ea830d0a438ff07662
kienhuynh279 <kienhuynh279@gmail.com>
2020-07-21 14:00:16 +0700

    update

--- public/js/app.js
+++ public/js/app.js
@@ -50560,8 +50560,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! F:\laravel\kien_hai_team\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! F:\laravel\kien_hai_team\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\laravel\kien-hai-project\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\laravel\kien-hai-project\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
